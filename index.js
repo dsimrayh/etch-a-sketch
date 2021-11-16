2f36bfe7816e51a2ee29424f7e6fa546666b3e22
Damian <dsimrayh@yahoo.com>
2021-11-15 18:19:01 -0800

    MVP Completed

---
+++ index.js
@@ -0,0 +1,40 @@
+function drawGrid(squares = 16) {
+    const container = document.querySelector('.container');
+    container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+    const ratio = 480 / squares;
+    for(i = 0; i < squares * squares; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${ratio}px`;
+        square.style.height = `${ratio}px`;
+        container.appendChild(square);
+    }
+
+    const divs = document.querySelectorAll('.square');
+    divs.forEach(div => {
+        div.addEventListener('mouseenter', (e) => {
+            e.target.classList.add('active');
+        });
+    });
+}
+
+function removeGrid() {
+    const container = document.querySelector('.container');
+    while(container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+function getUserInput() {
+    const userInput = prompt(
+        "Enter the number of squares per side\nDefault: 16 | Max: 100"
+        );
+    if(userInput > 100) {getUserInput()}
+    removeGrid();
+    drawGrid(+userInput);
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', getUserInput);
+
+drawGrid();
